cbc9eb2346a230f31e068fdee38cf4060985fb5e
lukwesolowski <luklgmobile@gmail.com>
2017-05-29 19:15:03 +0200

    #LW - dodanie tabeli Zarejestrowane Wizyty do bazy danych

---
+++ MedWeb/MedWeb.DA/Tables/RegisteredVisit.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MedWeb.DA.Tables
+{
+    class RegisteredVisit
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Key]
+        public DateTime Date { get; set; }
+        public DateTime Time { get; set; }
+        public string Complaint { get; set; }
+
+        [ForeignKey(nameof(Patient))]
+        public virtual Patient Patient { get; set; }
+
+        [ForeignKey(nameof(Doctor))]
+        public virtual Doctor Doctor { get; set; }
+    }
+}
