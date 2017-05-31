d37e6c28830f54a75be2a8ab33eb1258895c961e
lukwesolowski <luklgmobile@gmail.com>
2017-05-31 21:45:32 +0200

    #LW - usprawnienie modelu specjalizacji lekarzy

--- MedWeb/MedWeb.Web/Models/SpecializationViewModel.cs
+++ MedWeb/MedWeb.Web/Models/SpecializationViewModel.cs
@@ -1,11 +1,16 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace MedWeb.Web.Models
 {
     public class SpecializationViewModel
     {
         public int Id { get; set; }
+
+        [Display(Name = "Nazwa specjalizacji")]
         public string Name { get; set; }
+
+        [Display(Name = "Data dodania")]
         public DateTime InsertTime { get; set; }
     }
 }
