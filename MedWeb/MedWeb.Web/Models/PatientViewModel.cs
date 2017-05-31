79362769a6f8c1b0a7fb96f726a2ea96868cb957
lukwesolowski <luklgmobile@gmail.com>
2017-05-31 21:31:34 +0200

    #LW - usprawnienie modelu pacjentów

--- MedWeb/MedWeb.Web/Models/PatientViewModel.cs
+++ MedWeb/MedWeb.Web/Models/PatientViewModel.cs
@@ -1,17 +1,34 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace MedWeb.Web.Models
 {
     public class PatientViewModel
     {
         public int Id { get; set; }
+
+        [Display(Name = "Imię")]
         public string FirstName { get; set; }
+
+        [Display(Name = "Nazwisko")]
         public string LastName { get; set; }
+
+        [Display(Name = "Numer pesel")]
         public int Pesel { get; set; }
+
+        [Display(Name = "Data urodziń")]
         public DateTime BirthDate { get; set; }
+
+        [Display(Name = "Miasto")]
         public string City { get; set; }
+
+        [Display(Name = "Ulica")]
         public string Street { get; set; }
+
+        [Display(Name = "Numer domu")]
         public string HouseNumber { get; set; }
+
+        [Display(Name = "Kod pocztowy")]
         public string ZipCode { get; set; }
     }
 }
