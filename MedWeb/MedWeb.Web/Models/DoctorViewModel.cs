ee39557bae9e9577c3354063ac37d0d3db26f3e6
lukwesolowski <luklgmobile@gmail.com>
2017-05-31 21:26:53 +0200

    #LW - usprawnienie kontrollera lekarzy

--- MedWeb/MedWeb.Web/Models/DoctorViewModel.cs
+++ MedWeb/MedWeb.Web/Models/DoctorViewModel.cs
@@ -1,10 +1,16 @@
-﻿namespace MedWeb.Web.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MedWeb.Web.Models
 {
     public class DoctorViewModel
     {
         public int Id { get; set; }
         public int SpecializationId { get; set; }
+
+        [Display(Name = "Imię")]
         public string FirstName { get; set; }
+
+        [Display(Name = "Nazwisko")]
         public string LastName { get; set; }
     }
 }
