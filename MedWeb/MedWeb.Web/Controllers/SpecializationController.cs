6bd9eafe624cfad46052cd64c6c5ac167f9ddbff
lukwesolowski <luklgmobile@gmail.com>
2017-05-31 21:18:12 +0200

    #LW - przygotowanie kontrolera specjalizacji lekarzy

--- MedWeb/MedWeb.Web/Controllers/SpecializationController.cs
+++ MedWeb/MedWeb.Web/Controllers/SpecializationController.cs
@@ -1,13 +1,35 @@
-﻿using System.Web.Mvc;
+﻿using MedWeb.DA.Interfaces;
+using System.Web.Mvc;
 
 namespace MedWeb.Web.Controllers
 {
     public class SpecializationController : Controller
     {
-        // GET: Specialization
-        public ActionResult Index()
+        private ISpecializationRepository _specializationRepository;
+
+        private SpecializationController(ISpecializationRepository specializationRepository)
         {
-            return View();
+            _specializationRepository = specializationRepository;
+        }
+
+        public ActionResult ListOfSpecializations()
+        {
+            return Content("List Of Specializations");
+        }
+
+        public ActionResult ReadOnlyListOfSpecializations()
+        {
+            return Content("Read Only List Of Specializations");
+        }
+
+        public ActionResult DetailsOfSpecialization()
+        {
+            return Content("Details Of Specialization");
+        }
+
+        public ActionResult SpecializationForm()
+        {
+            return Content("Specialization Form");
         }
     }
 }
