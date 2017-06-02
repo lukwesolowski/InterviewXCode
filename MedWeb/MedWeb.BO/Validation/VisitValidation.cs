dd07d8ab33bfb047f65d79ce3cd81ff5caa2650d
lukwesolowski <luklgmobile@gmail.com>
2017-06-02 22:48:13 +0200

    #LW - uzupelnienie walidatora wizyt o kolejne reguły

--- MedWeb/MedWeb.BO/Validation/VisitValidation.cs
+++ MedWeb/MedWeb.BO/Validation/VisitValidation.cs
@@ -9,22 +9,25 @@ namespace MedWeb.BO.Validation
 {
     public class VisitValidation : AbstractValidator<RegisteredVisit>
     {
-        private readonly List<RegisteredVisit> _registeredVisits;
+        public readonly List<RegisteredVisit> _registeredVisits;
 
         public VisitValidation(List<RegisteredVisit> registeredVisits)
         {
             _registeredVisits = registeredVisits;
+
             RuleFor(x => x.DateTime)
                 .NotEmpty().WithMessage("Wypełnienie daty jest wymagane.")
                 .Must(IsDateUniqueForSpecificDoctor).WithMessage("Wizyta o bieżącej dacie już istnieje");
-            
+            RuleFor(x => x.Patient)
+                .NotEmpty().WithMessage("Obowiązkowe jest dodanie pacjenta do wizyty");
+            RuleFor(x => x.Doctor)
+                .NotEmpty().WithMessage("Obowiązkowe jest dodanie lekarza do wizyty");       
         }
 
         public bool IsDateUniqueForSpecificDoctor(RegisteredVisit editedOrNewVisit, DateTime newValue)
         {
             return _registeredVisits.All(x =>
-                x.DateTime != newValue || x.Doctor.Equals(editedOrNewVisit.Doctor));
-            
+                x.DateTime != newValue || x.Doctor.Equals(editedOrNewVisit.Doctor));       
         }
     }
 }
