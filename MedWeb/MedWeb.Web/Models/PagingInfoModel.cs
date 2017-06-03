f26e711bf88ff22d855a36ac8f26057a31a28ba5
lukwesolowski <luklgmobile@gmail.com>
2017-06-03 14:29:22 +0200

    #LW - dodanie klasy odpowiedzialnej za paginację

---
+++ MedWeb/MedWeb.Web/Models/PagingInfoModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MedWeb.Web.Models
+{
+    public class PagingInfoModel
+    {
+        public int TotalItems { get; set; }
+        public int ItemsOnPage { get; set; }
+        public int CurrentPage { get; set; }
+
+        public int CountTotalPages
+        {
+            get { return (int)Math.Ceiling((decimal)TotalItems / ItemsOnPage); }
+        }
+    }
+}
