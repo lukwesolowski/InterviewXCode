e546f0b75f122f02b7405f5cc1966cc24d468224
lukwesolowski <luklgmobile@gmail.com>
2017-06-03 15:01:56 +0200

    #LW - model odpowiedzialny za paginację stron

--- MedWeb/MedWeb.Web/Models/PagingInfoModel.cs
+++ MedWeb/MedWeb.Web/Models/PagingInfoModel.cs
@@ -1,16 +1,47 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace MedWeb.Web.Models
 {
     public class PagingInfoModel
     {
-        public int TotalItems { get; set; }
-        public int ItemsOnPage { get; set; }
-        public int CurrentPage { get; set; }
+        public List<string> Items { get; set; }
+        public PageInfo PageInfo{ get; set; }
+    }
 
-        public int CountTotalPages
+    public class PageInfo
+    {
+        public int NumOfItems { get; set; }
+        public int CurrentPage { get; set; }
+        public int PageSize { get; set; }
+        public int NumOfPages { get; set; }
+        public int StartPage { get; set; }
+        public int EndPage { get; set; }
+
+        public PageInfo(int numOfItems, int? page, int pageSize = 5)
         {
-            get { return (int)Math.Ceiling((decimal)TotalItems / ItemsOnPage); }
+            var numOfPages = (int)Math.Ceiling((decimal)numOfItems / (decimal)pageSize);
+            var currentPage = page != null ? (int)page : 1;
+            var startPage = 1;
+            var endPage = numOfPages;
+            if (startPage <= 0)
+            {
+                endPage -= (startPage - 1);
+                startPage = 1;
+            }
+            if (endPage > numOfPages)
+            {
+                endPage = numOfPages;
+                if (endPage > pageSize)
+                    startPage = endPage - (pageSize - 1);
+            }
+
+            NumOfItems = numOfItems;
+            CurrentPage = currentPage;
+            PageSize = pageSize;
+            NumOfPages = numOfPages;
+            StartPage = startPage;
+            EndPage = EndPage;
         }
     }
 }
