9a99b9f5a2f69ad8373fa4b48b5bbfe6feb6d35c
lukwesolowski <luklgmobile@gmail.com>
2017-06-03 15:42:24 +0200

    #LW - utworzenie nowe modelu do paginacji wizyt

---
+++ MedWeb/MedWeb.Web/Models/PagingVisitViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace MedWeb.Web.Models
+{
+    public class PagingVisitViewModel
+    {
+        public List<RegisteredVisitViewModel> RegisteredVisits { get; set; }
+        public PagingInfoModel PagingInfoModel { get; set; }
+    }
+}
