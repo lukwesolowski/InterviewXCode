2557b409320c8cdcdb3c505493703cd19e1948f2
lukwesolowski <luklgmobile@gmail.com>
2017-06-06 01:27:59 +0200

    #LW - kontroler lekarzy, podpięcie interfejsu repo lekarzy poprzez autofac

---
+++ MedWeb/MedWeb.Web/Controllers/DoctorController.cs
@@ -0,0 +1,23 @@
+﻿using Autofac;
+using MedWeb.DA.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MedWeb.Web.Controllers
+{
+    public class DoctorController : Controller
+    {
+        private IDoctorRepository _doctorRepository;
+
+        public DoctorController()
+        {
+            using (ILifetimeScope scope = MvcApplication.DepedencyResolver.BeginLifetimeScope())
+            {
+                _doctorRepository = scope.Resolve<IDoctorRepository>();
+            }
+        }
+    }
+}
